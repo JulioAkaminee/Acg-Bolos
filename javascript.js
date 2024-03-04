02e46a314178cc37556eb96e9cddb18cd072f049
JulioAkamine <95776461+JulioAkaminee@users.noreply.github.com>
2024-03-04 04:27:12 -0300

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function clickMenu() {
+    if (menuburguer.style.display == 'block') {
+        menuburguer.style.display = 'none'
+        document.body.style.overflow = 'auto'
+    } else {
+        menuburguer.style.display = 'block'
+        document.body.style.overflow = 'hidden'
+    } 
+
+   
+
+   }
